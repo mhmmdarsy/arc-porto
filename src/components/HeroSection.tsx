1db0f6ce4dc108b7203dae4683b9e0ebdf39c3c8
Muhammad Arsy Al-Fahd <muhammadarsyalfahd@gmail.com>
2025-12-22 15:30:40 +0800

    fix: adjust padding in HeroSection for improved layout

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,7 +3,7 @@ import { HeroDescription } from './HeroDescription';
 
 export function HeroSection() {
   return (
-    <section className="min-h-screen pt-24 px-6">
+    <section className="min-h-screen pt-96 px-6">
       <div className="max-w-screen-2xl mx-auto">
         <div className="grid grid-cols-1 lg:grid-cols-12 items-start">
           <HeroTitle />
