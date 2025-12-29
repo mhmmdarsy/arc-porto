81f17bb62864cf2353c3a3026e44988f34301e91
Muhammad Arsy Al-Fahd <muhammadarsyalfahd@gmail.com>
2025-12-29 22:44:38 +0800

    refactor: improve formatting and structure in HeroTitle component

--- src/components/sections/hero/HeroTitle.tsx
+++ src/components/sections/hero/HeroTitle.tsx
@@ -2,12 +2,14 @@ import { useTheme } from '../../../contexts/ThemeContext';
 
 export function HeroTitle() {
   const { isDark } = useTheme();
-  
+
   return (
     <div className="lg:col-span-8">
       <div className="relative">
-        <h1 
-          className={`text-[16rem] leading-none tracking-tighter transition-colors ${isDark ? 'text-white' : 'text-black'}`}
+        <h1
+          className={`text-[16rem] leading-none tracking-tighter transition-colors ${
+            isDark ? 'text-white' : 'text-black'
+          }`}
           style={{ fontWeight: 900, wordSpacing: '-1.5em', lineHeight: '0.8' }}
         >
           WEB DEVELOPER
