b2aaa6489a3d085fe26d4f89161d4cb2b145d2f4
Muhammad Arsy Al-Fahd <muhammadarsyalfahd@gmail.com>
2026-03-03 15:02:40 +0800

    feat: add MenuToggle component for responsive navigation

--- src/components/CardNav/MenuToggle.tsx
+++ src/components/CardNav/MenuToggle.tsx
@@ -1,6 +1,11 @@
 import { HiOutlineBars3, HiOutlineXMark } from 'react-icons/hi2';
 
-const MenuToggle = ({ isOpen, onToggle }) => (
+interface MenuToggleProps {
+  isOpen: boolean;
+  onToggle: () => void;
+}
+
+const MenuToggle = ({ isOpen, onToggle }: MenuToggleProps) => (
   <button
     onClick={onToggle}
     className="flex h-10 w-10 items-center justify-center rounded-full text-current transition hover:bg-black/5"
